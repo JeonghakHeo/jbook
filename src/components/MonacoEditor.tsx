a1443fcf2a5c34f38b20ad7e22b3a2bfe4b573d8
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-07 11:22:57 +1000

    Add code formatting with prettier

--- src/components/MonacoEditor.tsx
+++ src/components/MonacoEditor.tsx
@@ -1,22 +1,62 @@
-import MonacoEditor from '@monaco-editor/react'
+import { useRef } from 'react'
+import MonacoEditor, { type EditorDidMount } from '@monaco-editor/react'
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
+interface CodeEditorProps {
+  initialValue: string
+  onChange(value: string): void
+}
 
-const CodeEditor = () => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>()
+
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue())
+    })
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+  }
+
+  const onFormatClick = () => {
+    // get current value from editor
+    const unformatted = editorRef.current.getModel().getValue()
+
+    // format the value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    })
+
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted)
+  }
+
   return (
-    <MonacoEditor
-      language='javascript'
-      theme='dark'
-      height='500px'
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        language='javascript'
+        theme='dark'
+        height='500px'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
   )
 }
 
