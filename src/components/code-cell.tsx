f828a6085ddb30fcff5a66297a03a7db65b2e6ce
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-11 15:56:30 +1000

    Style TextEditor component

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import CodeEditor from './code-editor'
 import Preview from './preview'
 import bundle from '../bundler'
-import Resizable from '../resizable'
+import Resizable from './resizable'
 
 const CodeCell = () => {
   const [code, setCode] = useState('')
