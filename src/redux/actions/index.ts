08b6fdd536ed1e6c2098f51f147e1ce20c7e1fda
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-18 22:50:32 +1000

    Added action creators

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,20 +1,21 @@
 import { ActionType } from '../action-types'
 import { CellTypes } from '../cell'
 
-interface MoveCellAction {
+export type Direction = 'up' | 'down'
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL
   payload: {
     id: string
-    direction: 'up' | 'down'
+    direction: Direction
   }
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL
   payload: string
 }
 
-interface InsertCellBeforeCellAction {
+export interface InsertCellBeforeCellAction {
   type: ActionType.INSERT_CELL_BEFORE
   payload: {
     id: string
@@ -22,7 +23,7 @@ interface InsertCellBeforeCellAction {
   }
 }
 
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL
   payload: {
     id: string
